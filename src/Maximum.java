f935bed98a49c338540f1c790d12d947f7a65fb8
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-24 13:31:27 +0530

    Added Integer Max Method

--- src/Maximum.java
+++ src/Maximum.java
@@ -1,6 +1,20 @@
 public class Maximum {
+    public static <T extends Comparable<T> > T max(T x,T y, T z)
+    {
+        T max =x;
+        if(y.compareTo(max)>0)
+        {
+            max=y;
+        }
+        if(z.compareTo(max)>0)
+        {
+            max=z;
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to The Generics Problem");
+        Integer x=10,y=11,z=2;
+        System.out.println(Maximum.max(x,y,z));
     }
 }
