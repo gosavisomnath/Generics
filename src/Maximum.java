8c1721b7eebd8eb59434b7b382b899cc54559949
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-24 14:59:40 +0530

    Print Max Method has been added

--- src/Maximum.java
+++ src/Maximum.java
@@ -1,5 +1,14 @@
-public class Maximum {
-    public static <T extends Comparable<T> > T max(T x,T y, T z)
+public class Maximum<T extends  Comparable<T>> {
+    T x,y,z;
+
+    public Maximum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    public static <T extends Comparable<T> > T max(T x, T y, T z)
     {
         T max =x;
         if(y.compareTo(max)>0)
@@ -10,17 +19,22 @@ public class Maximum {
         {
             max=z;
         }
+        printMax(x,y,z,max);
         return max;
+    }
+    public static <T> void printMax(T x,T y,T z,T max)
+    {
+        System.out.printf("Max of %s,%s and %s is %s:",x,y,z,max);
     }
 
 
     public static void main(String[] args) {
         Integer x=10,y=11,z=2;
-        System.out.println(Maximum.max(x,y,z));
+        Maximum.max(x,y,z);
         Float p=2.2f,q=1.2f,r=5.2f;
-        System.out.println(Maximum.max(p,q,r));
+        Maximum.max(p,q,r);
         String d="abc",e="pune",f="Mumbai";
-        System.out.println(Maximum.max(d,e,f));
+        Maximum.max(d,e,f);
 
 
 
